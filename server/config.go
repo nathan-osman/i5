2d75337df609a0f773562f9052e3efcb0a626451
Nathan Osman <nathan@quickmediasolutions.com>
2019-05-25 18:49:20 -0700

    Initial server implementation.

--- server/config.go
+++ server/config.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"github.com/nathan-osman/i5/dockmon"
+)
+
+// Config provides the configuration for the i5 server.
+type Config struct {
+	// Addr indicates the address to be used for listening.
+	Addr string
+	// Dockmon is a pointer to a Dockmon instance.
+	Dockmon *dockmon.Dockmon
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,62 @@
 package server
+
+import (
+	"net"
+	"net/http"
+	"sync"
+
+	"github.com/nathan-osman/i5/service"
+	"github.com/sirupsen/logrus"
+)
+
+// Server listens for incoming connections and routes them accordingly.
+type Server struct {
+	mutex      sync.RWMutex
+	log        *logrus.Entry
+	listener   net.Listener
+	domainMap  map[string]*service.Service
+	closedChan chan bool
+}
+
+func (s *Server) handle(w http.ResponseWriter, r *http.Request) {
+	//...
+}
+
+func (s *Server) serve() {
+	defer close(s.closedChan)
+	defer s.log.Info("server stopped")
+	s.log.Info("server started")
+	server := http.Server{
+		Handler: http.HandlerFunc(s.handle),
+	}
+	for {
+		if err := server.Serve(s.listener); err == http.ErrServerClosed {
+			return
+		} else {
+			s.log.Error(err)
+		}
+	}
+}
+
+// New creates a new server from the provided configuration.
+func New(cfg *Config) (*Server, error) {
+	l, err := net.Listen("tcp", cfg.Addr)
+	if err != nil {
+		return nil, err
+	}
+	s := &Server{
+		log:        logrus.WithField("context", "server"),
+		listener:   l,
+		domainMap:  map[string]*service.Service{},
+		closedChan: make(chan bool),
+	}
+	go s.serve()
+	//...
+	return s, nil
+}
+
+// Close shuts down the server.
+func (s *Server) Close() {
+	_ = s.listener.Close()
+	<-s.closedChan
+}
