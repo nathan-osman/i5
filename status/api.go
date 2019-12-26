c2a46011421e3ec26a51c2fa3f524e44878b6887
Nathan Osman <nathan@quickmediasolutions.com>
2019-12-26 14:32:02 -0800

    Sort containers by name.

--- status/api.go
+++ status/api.go
@@ -2,8 +2,10 @@ package status
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/go-chi/render"
+	"github.com/nathan-osman/i5/conman"
 )
 
 type statusResponse struct {
@@ -29,13 +31,23 @@ func (c *containerResponse) Render(w http.ResponseWriter, r *http.Request) error
 	return nil
 }
 
+type byName []*conman.Info
+
+func (n byName) Len() int           { return len(n) }
+func (n byName) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n byName) Less(i, j int) bool { return n[i].Name < n[j].Name }
+
 func (s *Status) getContainers(w http.ResponseWriter, r *http.Request) {
-	containers := []render.Renderer{}
-	for _, c := range s.conman.Info() {
-		containers = append(containers, &containerResponse{
+	var (
+		containers    = s.conman.Info()
+		containerList = []render.Renderer{}
+	)
+	sort.Sort(byName(containers))
+	for _, c := range containers {
+		containerList = append(containerList, &containerResponse{
 			Name:    c.Name,
 			Running: c.Running,
 		})
 	}
-	render.RenderList(w, r, containers)
+	render.RenderList(w, r, containerList)
 }
