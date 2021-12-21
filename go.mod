e1f9ff705860685b6c6d937c22a9cc66d4fd9b18
Nathan Osman <nathan@quickmediasolutions.com>
2021-12-20 21:48:23 -0800

    Switch to cli v2 and add createuser command.

--- go.mod
+++ go.mod
@@ -9,12 +9,13 @@ require (
 	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/render v1.0.1
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/howeyc/gopass v0.0.0-20210920133722-c8aef6fb66ef
 	github.com/lib/pq v1.10.4
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.8.1
-	github.com/urfave/cli v1.22.5
+	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 	google.golang.org/grpc v1.43.0 // indirect
-	gorm.io/driver/sqlite v1.2.6 // indirect
+	gorm.io/driver/sqlite v1.2.6
 	gorm.io/gorm v1.22.4
 )
--- main.go
+++ main.go
@@ -6,210 +6,249 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/howeyc/gopass"
 	"github.com/nathan-osman/i5/conman"
 	"github.com/nathan-osman/i5/db"
 	"github.com/nathan-osman/i5/dockmon"
 	"github.com/nathan-osman/i5/server"
 	"github.com/nathan-osman/i5/status"
-	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
+	statusDB "github.com/nathan-osman/i5/status/db"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "i5"
-	app.Usage = "reverse proxy for Docker containers"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   "debug",
-			EnvVar: "DEBUG",
-			Usage:  "enable debug mode",
+	app := &cli.App{
+		Name:  "i5",
+		Usage: "reverse proxy for Docker containers",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "debug",
+				EnvVars: []string{"DEBUG"},
+				Usage:   "enable debug mode",
+			},
+			&cli.StringFlag{
+				Name:    "docker-host",
+				Value:   "unix:///var/run/docker.sock",
+				EnvVars: []string{"DOCKER_HOST"},
+				Usage:   "host running Docker",
+			},
+			&cli.StringFlag{
+				Name:    "email",
+				EnvVars: []string{"EMAIL"},
+				Usage:   "email address to use for challenges",
+			},
+			&cli.StringFlag{
+				Name:    "http-addr",
+				Value:   ":http",
+				EnvVars: []string{"HTTP_ADDR"},
+				Usage:   "HTTP address to listen on",
+			},
+			&cli.StringFlag{
+				Name:    "https-addr",
+				Value:   ":https",
+				EnvVars: []string{"HTTPS_ADDR"},
+				Usage:   "HTTPS address to listen on",
+			},
+			&cli.BoolFlag{
+				Name:    "mysql",
+				EnvVars: []string{"MYSQL"},
+				Usage:   "connect to MySQL",
+			},
+			&cli.IntFlag{
+				Name:    "mysql-port",
+				Value:   3306,
+				EnvVars: []string{"MYSQL_PORT"},
+				Usage:   "port for MySQL server",
+			},
+			&cli.StringFlag{
+				Name:    "mysql-host",
+				Value:   "mysql",
+				EnvVars: []string{"MYSQL_HOST"},
+				Usage:   "hostname of MySQL server",
+			},
+			&cli.StringFlag{
+				Name:    "mysql-user",
+				Value:   "root",
+				EnvVars: []string{"MYSQL_USER"},
+				Usage:   "username for connecting to MySQL",
+			},
+			&cli.StringFlag{
+				Name:    "mysql-password",
+				EnvVars: []string{"MYSQL_PASSWORD"},
+				Usage:   "password for connecting to MySQL",
+			},
+			&cli.BoolFlag{
+				Name:    "postgres",
+				EnvVars: []string{"POSTGRES"},
+				Usage:   "connect to PostgreSQL",
+			},
+			&cli.IntFlag{
+				Name:    "postgres-port",
+				Value:   5432,
+				EnvVars: []string{"POSTGRES_PORT"},
+				Usage:   "port for PostgreSQL server",
+			},
+			&cli.StringFlag{
+				Name:    "postgres-host",
+				Value:   "postgres",
+				EnvVars: []string{"POSTGRES_HOST"},
+				Usage:   "hostname of PostgreSQL server",
+			},
+			&cli.StringFlag{
+				Name:    "postgres-user",
+				Value:   "postgres",
+				EnvVars: []string{"POSTGRES_USER"},
+				Usage:   "username for connecting to PostgreSQL",
+			},
+			&cli.StringFlag{
+				Name:    "postgres-password",
+				EnvVars: []string{"POSTGRES_PASSWORD"},
+				Usage:   "password for connecting to PostgreSQL",
+			},
+			&cli.StringFlag{
+				Name:    "status-domain",
+				EnvVars: []string{"STATUS_DOMAIN"},
+				Usage:   "domain name for internal server",
+			},
+			&cli.BoolFlag{
+				Name:    "status-insecure",
+				EnvVars: []string{"STATUS_INSECURE"},
+				Usage:   "allow insecure connections to the internal server",
+			},
+			&cli.StringFlag{
+				Name:    "storage-dir",
+				EnvVars: []string{"STORAGE_DIR"},
+				Usage:   "directory for storing files related to i5",
+			},
 		},
-		cli.StringFlag{
-			Name:   "docker-host",
-			Value:  "unix:///var/run/docker.sock",
-			EnvVar: "DOCKER_HOST",
-			Usage:  "host running Docker",
-		},
-		cli.StringFlag{
-			Name:   "email",
-			EnvVar: "EMAIL",
-			Usage:  "email address to use for challenges",
-		},
-		cli.StringFlag{
-			Name:   "http-addr",
-			Value:  ":http",
-			EnvVar: "HTTP_ADDR",
-			Usage:  "HTTP address to listen on",
-		},
-		cli.StringFlag{
-			Name:   "https-addr",
-			Value:  ":https",
-			EnvVar: "HTTPS_ADDR",
-			Usage:  "HTTPS address to listen on",
-		},
-		cli.BoolFlag{
-			Name:   "mysql",
-			EnvVar: "MYSQL",
-			Usage:  "connect to MySQL",
-		},
-		cli.IntFlag{
-			Name:   "mysql-port",
-			Value:  3306,
-			EnvVar: "MYSQL_PORT",
-			Usage:  "port for MySQL server",
-		},
-		cli.StringFlag{
-			Name:   "mysql-host",
-			Value:  "mysql",
-			EnvVar: "MYSQL_HOST",
-			Usage:  "hostname of MySQL server",
-		},
-		cli.StringFlag{
-			Name:   "mysql-user",
-			Value:  "root",
-			EnvVar: "MYSQL_USER",
-			Usage:  "username for connecting to MySQL",
-		},
-		cli.StringFlag{
-			Name:   "mysql-password",
-			EnvVar: "MYSQL_PASSWORD",
-			Usage:  "password for connecting to MySQL",
-		},
-		cli.BoolFlag{
-			Name:   "postgres",
-			EnvVar: "POSTGRES",
-			Usage:  "connect to PostgreSQL",
-		},
-		cli.IntFlag{
-			Name:   "postgres-port",
-			Value:  5432,
-			EnvVar: "POSTGRES_PORT",
-			Usage:  "port for PostgreSQL server",
-		},
-		cli.StringFlag{
-			Name:   "postgres-host",
-			Value:  "postgres",
-			EnvVar: "POSTGRES_HOST",
-			Usage:  "hostname of PostgreSQL server",
-		},
-		cli.StringFlag{
-			Name:   "postgres-user",
-			Value:  "postgres",
-			EnvVar: "POSTGRES_USER",
-			Usage:  "username for connecting to PostgreSQL",
-		},
-		cli.StringFlag{
-			Name:   "postgres-password",
-			EnvVar: "POSTGRES_PASSWORD",
-			Usage:  "password for connecting to PostgreSQL",
-		},
-		cli.StringFlag{
-			Name:   "status-domain",
-			EnvVar: "STATUS_DOMAIN",
-			Usage:  "domain name for internal server",
-		},
-		cli.BoolFlag{
-			Name:   "status-insecure",
-			EnvVar: "STATUS_INSECURE",
-			Usage:  "allow insecure connections to the internal server",
-		},
-		cli.StringFlag{
-			Name:   "storage-dir",
-			EnvVar: "STORAGE_DIR",
-			Usage:  "directory for storing files related to i5",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
+		Commands: []*cli.Command{
+			{
+				Name:  "createuser",
+				Usage: "create a new user account",
+				Action: func(c *cli.Context) error {
 
-		// Enable debug logging if requested
-		if c.Bool("debug") {
-			logrus.SetLevel(logrus.DebugLevel)
-		}
+					// Open the local database
+					d, err := statusDB.New(c.String("storage-dir"))
+					if err != nil {
+						return err
+					}
+					defer d.Close()
 
-		// Create the Docker monitor
-		dm, err := dockmon.New(&dockmon.Config{
-			Host: c.String("docker-host"),
-		})
-		if err != nil {
-			return err
-		}
-		defer dm.Close()
+					// Prompt for the username
+					var username string
+					fmt.Print("Username? ")
+					fmt.Scanln(&username)
 
-		// Create the database manager
-		dbman := db.NewManager()
+					// Prompt for the password, hiding the input
+					fmt.Print("Password? ")
+					p, err := gopass.GetPasswd()
+					if err != nil {
+						return err
+					}
 
-		// Connect to MySQL if requested
-		if c.Bool("mysql") {
-			msql, err := db.NewMySQL(
-				c.String("mysql-host"),
-				c.Int("mysql-port"),
-				c.String("mysql-user"),
-				c.String("mysql-password"),
-			)
-			if err != nil {
-				return err
-			}
-			dbman.Register(msql)
-		}
+					// Create the user and set the password
+					u := &statusDB.User{Username: username}
+					if err := u.SetPassword(string(p)); err != nil {
+						return err
+					}
 
-		// Connect to PostgreSQL if requested
-		if c.Bool("postgres") {
-			psql, err := db.NewPostgres(
-				c.String("postgres-host"),
-				c.Int("postgres-port"),
-				c.String("postgres-user"),
-				c.String("postgres-password"),
-			)
-			if err != nil {
-				return err
-			}
-			dbman.Register(psql)
-		}
+					// Save the user to the database
+					if err := d.Save(u).Error; err != nil {
+						return err
+					}
 
-		// Create the container manager
-		cm := conman.New(&conman.Config{
-			EventChan: dm.EventChan,
-			Dbman:     dbman,
-		})
-		defer cm.Close()
+					fmt.Println("New user created successfully.")
 
-		// If a domain name for the internal server was specified, use it
-		if statusDomain := c.String("status-domain"); statusDomain != "" {
-			s, err := status.New(&status.Config{
-				Domain:     statusDomain,
-				Insecure:   c.Bool("status-insecure"),
-				StorageDir: c.String("storage-dir"),
-				Conman:     cm,
-				Dbman:      dbman,
+					return nil
+				},
+			},
+		},
+		Action: func(c *cli.Context) error {
+
+			// Create the Docker monitor
+			dm, err := dockmon.New(&dockmon.Config{
+				Host: c.String("docker-host"),
 			})
 			if err != nil {
 				return err
 			}
-			defer s.Close()
-			cm.Add(s.Container)
-		}
+			defer dm.Close()
 
-		// Create the server
-		sv, err := server.New(&server.Config{
-			Debug:      c.Bool("debug"),
-			Email:      c.String("email"),
-			HTTPAddr:   c.String("http-addr"),
-			HTTPSAddr:  c.String("https-addr"),
-			StorageDir: c.String("storage-dir"),
-			Conman:     cm,
-		})
-		if err != nil {
-			return err
-		}
-		defer sv.Close()
+			// Create the database manager
+			dbman := db.NewManager()
 
-		// Wait for SIGINT or SIGTERM
-		sigChan := make(chan os.Signal)
-		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-		<-sigChan
+			// Connect to MySQL if requested
+			if c.Bool("mysql") {
+				msql, err := db.NewMySQL(
+					c.String("mysql-host"),
+					c.Int("mysql-port"),
+					c.String("mysql-user"),
+					c.String("mysql-password"),
+				)
+				if err != nil {
+					return err
+				}
+				dbman.Register(msql)
+			}
 
-		return nil
+			// Connect to PostgreSQL if requested
+			if c.Bool("postgres") {
+				psql, err := db.NewPostgres(
+					c.String("postgres-host"),
+					c.Int("postgres-port"),
+					c.String("postgres-user"),
+					c.String("postgres-password"),
+				)
+				if err != nil {
+					return err
+				}
+				dbman.Register(psql)
+			}
+
+			// Create the container manager
+			cm := conman.New(&conman.Config{
+				EventChan: dm.EventChan,
+				Dbman:     dbman,
+			})
+			defer cm.Close()
+
+			// If a domain name for the internal server was specified, use it
+			if statusDomain := c.String("status-domain"); statusDomain != "" {
+				s, err := status.New(&status.Config{
+					Domain:     statusDomain,
+					Insecure:   c.Bool("status-insecure"),
+					StorageDir: c.String("storage-dir"),
+					Conman:     cm,
+					Dbman:      dbman,
+				})
+				if err != nil {
+					return err
+				}
+				defer s.Close()
+				cm.Add(s.Container)
+			}
+
+			// Create the server
+			sv, err := server.New(&server.Config{
+				Debug:      c.Bool("debug"),
+				Email:      c.String("email"),
+				HTTPAddr:   c.String("http-addr"),
+				HTTPSAddr:  c.String("https-addr"),
+				StorageDir: c.String("storage-dir"),
+				Conman:     cm,
+			})
+			if err != nil {
+				return err
+			}
+			defer sv.Close()
+
+			// Wait for SIGINT or SIGTERM
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+			<-sigChan
+
+			return nil
+		},
 	}
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "fatal: %s\n", err.Error())
