d1fe2d888a9656183f63bbe65eefacb842a2157b
Nathan Osman <nathan@quickmediasolutions.com>
2022-03-19 18:18:56 -0700

    Redirect API requests to the local instance when running in the react development server.

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_PREFIX;
 
 ReactDOM.render(
   <React.StrictMode>
