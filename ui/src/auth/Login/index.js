bacfec88efb46c2a1e41fe4a7933fe9e0f7b6089
Nathan Osman <nathan@quickmediasolutions.com>
2022-03-23 21:02:59 -0700

    Begin implementing login form.

--- ui/src/auth/Login/index.js
+++ ui/src/auth/Login/index.js
@@ -1,6 +1,25 @@
+import './index.css';
+
 const Login = () => {
   return (
-    <p>Login Form</p>
+    <div id="Login" className="bg-secondary bg-gradient">
+      <div class="card text-dark bg-light mb-3" style={{ width: '18rem' }}>
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <form>
+            <div className="mb-3">
+              <label for="username" className="form-label">Username</label>
+              <input type="text" className="form-control" id="username" autoFocus />
+            </div>
+            <div className="mb-3">
+              <label for="password" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- ui/src/index.js
+++ ui/src/index.js
@@ -7,8 +7,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './app/App';
 import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './auth/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_PREFIX;
 
@@ -19,8 +20,8 @@ ReactDOM.render(
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
           </Route>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
