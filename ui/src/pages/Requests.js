5e58d41a4f933b279dca5836e1b1f46d09fc2b90
Nathan Osman <nathan@quickmediasolutions.com>
2022-04-09 15:02:51 -0700

    Improve request formatting.

--- ui/src/pages/Requests.js
+++ ui/src/pages/Requests.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Requests = () => {
 
-  const [request, setRequests] = useState([]);
+  const [requests, setRequests] = useState([]);
 
   useEffect(() => {
     const loc = window.location;
@@ -14,12 +14,18 @@ const Requests = () => {
       const data = JSON.parse(e.data);
       const date = new Date();
       const time =
-        `${date.getHours().padStart(2, '0')}:` +
-        `${date.getMinutes().padStart(2, '0')}:` +
-        `${date.getSeconds().padStart(2, '0')}`;
+        <>
+          {date.getHours().toString().padStart(2, '0')}:
+          {date.getMinutes().toString().padStart(2, '0')}:
+          {date.getSeconds().toString().padStart(2, '0')}
+          <small className="text-muted text-small">
+            .{date.getMilliseconds().toString().padStart(3, '0')}
+          </small>
+        </>;
       setRequests(r => [
         {
           ...data.data,
+          date,
           time
         },
         ...r
@@ -47,15 +53,22 @@ const Requests = () => {
           </tr>
         </thead>
         <tbody>
-          {request.map(request => (
-            <tr key={+ request.time}>
-              <td>{request.time}</td>
-              <td>{request.remote_addr}</td>
-              <td>
-                <span className="badge bg-secondary">{request.method}</span></td>
-              <td><strong>{request.host}</strong>{request.path}</td>
+          {requests.length ?
+            requests.map(request => (
+              <tr key={+ request.date}>
+                <td>{request.time}</td>
+                <td>{request.remote_addr}</td>
+                <td>
+                  <span className="badge bg-secondary">{request.method}</span></td>
+                <td><strong>{request.host}</strong>{request.path}</td>
+              </tr>
+            )) :
+            <tr>
+              <td colSpan="4">
+                <p className="text-muted text-center p-5">No requests yet</p>
+              </td>
             </tr>
-          ))}
+          }
         </tbody>
       </table>
     </div>
