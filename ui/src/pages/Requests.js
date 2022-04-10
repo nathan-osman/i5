a33d3b94b4d39178889ebaadbe621a4164a009cc
Nathan Osman <nathan@quickmediasolutions.com>
2022-04-09 20:52:39 -0700

    Fix column miss-count.

--- ui/src/pages/Requests.js
+++ ui/src/pages/Requests.js
@@ -83,7 +83,7 @@ const Requests = () => {
               </tr>
             )) :
             <tr>
-              <td colSpan="5">
+              <td colSpan="6">
                 <p className="text-muted text-center p-5">No requests yet</p>
               </td>
             </tr>
