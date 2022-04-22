cb0f4f310d81c72bab2b8e7d9759afdc8db9056c
Nathan Osman <nathan@quickmediasolutions.com>
2022-04-21 21:58:59 -0700

    Layout adjustments.

--- ui/pages/containers.js
+++ ui/pages/containers.js
@@ -1,9 +1,15 @@
-import Layout from "../components/layout";
+import Layout from "../components/layout"
 
 export default function Containers() {
+
+  //...
+
   return (
     <Layout>
-      <h1>Containers!</h1>
+      <p className="help">
+        This table displays a list of Docker containers currently being monitored by i5.
+        Containers in the running state are proxied unless maintenance mode is active.
+      </p>
     </Layout>
   )
 }
