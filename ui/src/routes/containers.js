878667a5f6f8a07f456186565a9bd8f834ca1837
Nathan Osman <nathan@quickmediasolutions.com>
2022-04-29 14:20:20 -0700

    Initial implementation of home page and containers page.

--- ui/src/routes/containers.js
+++ ui/src/routes/containers.js
@@ -1,5 +1,14 @@
+import ContainerList from '../components/containerlist'
+
 export default function Containers() {
+
   return (
-    <h1>Containers</h1>
+    <>
+      <div className="title">Containers</div>
+      <div className="secondary">
+        This page lists all containers running on the host that are recognized by i5 (containers with the <kbd>i5.*</kbd> set of tags). You can use the tools available on the right to control the containers.
+      </div>
+      <ContainerList />
+    </>
   )
 }
--- ui/src/routes/home.js
+++ ui/src/routes/home.js
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <div className="title">Home</div>
+      <div className="secondary">
+        i5 provides this web interface for interacting with containers and services from the host. You can also use this interface for monitoring services and requests, watching traffic in realtime.
+      </div>
+    </>
   )
 }
