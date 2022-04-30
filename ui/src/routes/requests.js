87772ac7469be1622ee1a98257255f9e64b55211
Nathan Osman <nathan@quickmediasolutions.com>
2022-04-29 21:25:24 -0700

    Basic WebSocket implementation.

--- ui/src/routes/requests.js
+++ ui/src/routes/requests.js
@@ -1,5 +1,36 @@
+import { useEffect, useMemo } from 'react'
+import { usePopup } from '../lib/popup'
+
 export default function Requests() {
+
+  const popup = usePopup()
+
+  const webSocket = useMemo(() => {
+    const secure = location.protocol.startsWith('https')
+    const webSocket = new WebSocket(
+      `${secure ? 'wss' : 'ws'}://${location.host}/api/ws`
+    )
+    webSocket.onopen = () => {
+      //...
+    }
+    webSocket.onerror = (e) => {
+      popup.error(e.message)
+    }
+    webSocket.onclose = () => {
+      popup.info("WebSocket connection lost.")
+    }
+    return webSocket
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      webSocket.close()
+    }
+  }, [])
+
   return (
-    <h1>Requests</h1>
+    <>
+      <div className="title">Requests</div>
+    </>
   )
 }
