abaac1a78a5377d79c2f6484057fdee4498ed96c
Nathan Osman <nathan@quickmediasolutions.com>
2022-05-05 23:09:32 -0700

    Update Logger to use new Container type.

--- logger/container.go
+++ logger/container.go
@@ -2,27 +2,54 @@ package logger
 
 import (
 	"github.com/nathan-osman/go-herald"
+	"github.com/nathan-osman/i5/container"
 )
 
-const messageContainerUpdateType = "container-update"
+const (
+	messageContainerCreatedType   = "container-created"
+	messageContainerDestroyedType = "container-destroyed"
+	messageContainerStartedType   = "container-started"
+	messageContainerStoppedType   = "container-stopped"
+)
 
-type messageContainerUpdate struct {
-	ID     string `json:"id"`
-	Action string `json:"action"`
-}
-
-func (l *Logger) LogContainerUpdate(id, action string) {
+func (l *Logger) logContainerAction(messageType string, v interface{}) {
 	if l.herald != nil {
 		m, err := herald.NewMessage(
-			messageContainerUpdateType,
-			&messageContainerUpdate{
-				ID:     id,
-				Action: action,
-			},
+			messageType,
+			v,
 		)
 		if err != nil {
 			// TODO: log error
 		}
 		l.herald.Send(m, nil)
 	}
+}
+
+func (l *Logger) LogContainerCreated(c *container.Container) {
+	l.logContainerAction(messageContainerCreatedType, c)
+}
+
+type messageContainerAction struct {
+	ID string `json:"id"`
+}
+
+func (l *Logger) LogContainerDestroyed(id string) {
+	l.logContainerAction(
+		messageContainerDestroyedType,
+		&messageContainerAction{ID: id},
+	)
+}
+
+func (l *Logger) LogContainerStarted(id string) {
+	l.logContainerAction(
+		messageContainerStartedType,
+		&messageContainerAction{ID: id},
+	)
+}
+
+func (l *Logger) LogContainerStopped(id string) {
+	l.logContainerAction(
+		messageContainerStoppedType,
+		&messageContainerAction{ID: id},
+	)
 }
