0ee80cb9ed34b1f6c17aba381260d03466949512
Nathan Osman <nathan@quickmediasolutions.com>
2022-08-01 10:32:07 -0700

    Implement reconnection for WebSocket.

--- ui/src/lib/websocket.js
+++ ui/src/lib/websocket.js
@@ -9,28 +9,43 @@ function WebSocketProvider({ children }) {
 
   const eventTarget = useMemo(() => new EventTarget(), [])
 
-  // TODO: automatic reconnection when connection is lost
-
   useEffect(() => {
     const secure = location.protocol.startsWith('https')
-    const webSocket = new WebSocket(
-      `${secure ? 'wss' : 'ws'}://${location.host}/api/ws`
-    )
-    webSocket.onmessage = (e) => {
-      const json = JSON.parse(e.data)
-      const event = new CustomEvent(json.type, { detail: json.data })
-      eventTarget.dispatchEvent(event)
-    }
-    webSocket.onerror = (e) => {
-      popup.error(e.message)
-    }
-    webSocket.onclose = (e) => {
-      if (!e.wasClean) {
-        popup.info("WebSocket connection lost")
+
+    let webSocket
+    let reconnectTimerID
+
+    function connect(reconnecting) {
+      webSocket = new WebSocket(
+        `${secure ? 'wss' : 'ws'}://${location.host}/api/ws`
+      )
+      webSocket.onopen = () => {
+        if (reconnecting) {
+          popup.info("reconnected to WebSocket")
+        }
+      }
+      webSocket.onmessage = (e) => {
+        const json = JSON.parse(e.data)
+        const event = new CustomEvent(json.type, { detail: json.data })
+        eventTarget.dispatchEvent(event)
+      }
+      webSocket.onerror = (e) => {
+        popup.error(e.message)
+      }
+      webSocket.onclose = (e) => {
+        if (!e.wasClean) {
+          popup.error("WebSocket disconnected; reconnecting...")
+          reconnectTimerID = setTimeout(() => connect(true), 30 * 1000)
+        }
       }
     }
+
+    // Make the initial connection
+    connect(false)
+
     return () => {
       webSocket.close()
+      clearTimeout(reconnectTimerID)
     }
   }, [])
 
